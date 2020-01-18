01af4b2c53bee86a9cf01e58539331a596040dc9
tolgatr61 <47320521+tolgatr61@users.noreply.github.com>
2020-01-19 00:33:23 +0100

    Config du main

--- Solveur-feature_tolga/solveur/Main.java
+++ Solveur-feature_tolga/solveur/Main.java
@@ -6,9 +6,21 @@ public class Main {
 			Grille g1=new Grille(16,16); //choix de taille de la grille
 			//test des murs pour éviter les conflits tout en commentaire pour le moment
 			
-			//Portal fin = new Portal(14,1,"blue",g1); // choix de la position et couleur du portail
-			//Bot botBlue = new Bot(1,1,"blue",g1); //Création du bot et ajout automatique à une grille existante (position, couleur, grille)
+			Portal fin = new Portal(14,1,"blue",g1); // choix de la position et couleur du portail
+			Bot botBlue = new Bot(1,1,"blue",g1); //Création du bot et ajout automatique à une grille existante (position, couleur, grille)
 			g1.afficher();
+			
+			Move goDroiteBlue = new Move(botBlue,"droite");
+			State state1 = new State(g1,goDroiteBlue); //Calcul de la prochaine carte avec un move donné
+			state1.getGrille().afficher();
+			
+			Move goBasBlue = new Move(botBlue,"bas");
+			State state2 = new State(state1.getGrille(),goBasBlue);
+			state2.getGrille().afficher();
+			
+			Move goGaucheBlue = new Move(botBlue,"gauche");
+			State state3 = new State(state2.getGrille(),goGaucheBlue);
+			state3.getGrille().afficher();
 			
 			/*
 
