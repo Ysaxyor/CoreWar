fb9feed6ce48d0dcd7a541e261b23c66ee64f26a
Butterpod <59997173+Butterpod@users.noreply.github.com>
2020-01-31 12:16:06 +0100

    Update Main.java

--- Grid/Grid/Main.java
+++ Grid/Grid/Main.java
@@ -1,25 +1,19 @@
 package grid;
 
-<<<<<<< HEAD
-public class Main{
-	public static void main(String[] args) {
-		GUI test = new GUI();
-		test.repaint();
-	}
-
-
-=======
 public class Main {
+
 	public static void main(String[] args) {
 		GUI gui = new GUI();
 		gui.repaint();
-		
+
 	}
+
+	
+
 
 
 	
 
 
->>>>>>> ec2f62458e9b750bfa916c92bf8241b8428c8c24
-}
 
+}
