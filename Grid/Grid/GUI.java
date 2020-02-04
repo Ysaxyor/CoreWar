0ef7b0e055718850e86a444a52aab97fbb2e295c
Butterpod <59997173+Butterpod@users.noreply.github.com>
2020-02-04 14:41:13 +0100

    Update GUI.java

--- Grid/Grid/GUI.java
+++ Grid/Grid/GUI.java
@@ -8,14 +8,17 @@ import java.awt.*;
 
 
 public class GUI extends JFrame{
-	GridLayout grid = new GridLayout(24,24,3,4); // Grille 24x24 , espacé de 5 horizontalement et 4 verticalement
+
+	int spacing = 5; // Constante qui decide l'ecart entre chaque case
+	GridLayout grid = new GridLayout(20,20,4,3);
 
 	JFrame frame = new JFrame();
 	// La fenetre
 	public GUI(){
 		frame.setTitle("Solveur"); // Titre de la Fenetre
-		frame.setSize(1400,900); // Taille de la fenetre (Longueur, Hauteur)
+		frame.setSize(1500,1000); // Taille de la fenetre (Longueur, Hauteur)
 		frame.setMinimumSize(new Dimension(1286,829));
+		//frame.setExtendedState(frame.MAXIMIZED_BOTH);
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Lorsque on clique sur fermer, notre application est EXTERMINER
 		frame.setVisible(true); //permet que la fenetre soit visible
@@ -30,7 +33,15 @@ public class GUI extends JFrame{
 		frame.add(panel,BorderLayout.CENTER); // Panel de la grille centré
      		panel.setLayout(grid);
 
-		//
+		/////
+		
+		
+		//box.add( label1 );
+
+
+
+		//////
+
 		Component rigidareaN = Box.createRigidArea(new Dimension(200,200)); // "Mur invisible" Situé au Nord
 		Component rigidareaS = Box.createRigidArea(new Dimension(30,30));// "Mur invisible" Situé au Sud
 		Component rigidareaE = Box.createRigidArea(new Dimension(30,30));// "Mur invisible" Situé au EST
@@ -44,8 +55,16 @@ public class GUI extends JFrame{
 
 		
 
-		for (int i =0; i<576; i++){ // Boucle permettant de creer les cases
-				panel.add(new JButton()); // Ajout des case sur la grille
+		for (int i =0; i<400; i++){ // Boucle permettant de creer les cases
+				JLabel label1 = new JLabel( "mur" );  // Commentaire dans la box
+				label1.setMaximumSize(new Dimension(Integer.MAX_VALUE, label1.getMinimumSize().height)); // 
+				label1.setOpaque( true ); // Background du label
+				label1.setBackground( Color.GRAY ); // couleur du background du label
+				label1.setForeground( Color.BLACK ); // couleur du texte
+		
+				Box box = Box.createHorizontalBox(); 
+				box.add(label1);
+				panel.add(box); // Ajout des case sur la grille
 
 			}
 	}
