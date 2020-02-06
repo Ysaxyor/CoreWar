d576b946e1edb0aa1762046a249e9e68dd9d067a
BryanHuet <huetb050@gmail.com>
2020-02-06 12:41:25 +0100

    up main, ajout de commentaire

--- solveur/Main.java
+++ solveur/Main.java
@@ -5,27 +5,17 @@ public class Main {
 	public static void main(String[] args){
 
 			//GENERATION
-			Grille g1=new Grille(10,15); //choix de taille de la grille
-			Bot b1 = new Bot(1,1,"Blue");
-			Mirror mirrorBlue = new Mirror(1,8,"Blue",'h');
-			Mirror mirrorRed = new Mirror(7,1,"Red",'h');
-			Mirror mirrorRed2 = new Mirror(7,10,"Red",'t');
-			Goal goal = new Goal(8,10,"blue");
-			g1.addPiont(b1);
-			g1.addPiont(mirrorBlue);
-			g1.addPiont(mirrorRed);
-			g1.addPiont(goal);
-			g1.addPiont(mirrorRed2);
-			State etat = new State(g1);
-			etat.affiche();
-			Move test = new Move(b1,"bas");
-			etat.play(test).affiche();
-/*
-			Mirror mirrorBlue = new Mirror(1,8,"blue",'t',g1);
+			Grille grille=new Grille(18,26); //choix de taille de la grille
 
-			Bot botBlue = new Bot(1,1,"blue",g1); //Création du bot et ajout automatique à une grille existante (position, couleur, grille)
-			Goal goal = new Goal(8,10,"blue",g1);
-		//	g1.afficher();
+			//CREATION DES objets
+			Bot botBlue = new Bot(1,1,"Blue");
+			Mirror redMirror = new Mirror(5,1,"Red",'h');
+			Mirror blueMirror = new Mirror(5,5,"Blue",'t');
+
+			//ajout des objets à la grille
+			grille.addPiont(botBlue);
+			grille.addPiont(redMirror);
+			grille.addPiont(blueMirror);
 
 			//CREATION DES MOVES POSSIBLES
 			Move goDroiteBlue = new Move(botBlue,"droite"); //On détermine le mouvement d'un bot, peut-être réutiliser pour un même robot avec une même direction
@@ -33,8 +23,11 @@ public class Main {
 			Move goBasBlue = new Move(botBlue,"bas");
 			Move goHautBlue = new Move(botBlue,"haut");
 			Move goGaucheBlue = new Move(botBlue,"gauche");
-*/
-			//GENERATION DES ETATS ET TEST
 
+			//GENERATION DES ETATS ET TEST
+			State etat = new State(grille);
+			etat.affiche();
+			State etat2 = etat.play(goBasBlue);
+			etat2.affiche();
 	}
 }
