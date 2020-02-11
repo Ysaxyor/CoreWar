c548f567e09b5a9ae08fc226bff149aa4246510d
Butterpod <59997173+Butterpod@users.noreply.github.com>
2020-02-11 11:55:59 +0100

    Update GUI.java

--- Grid/Grid/GUI.java
+++ Grid/Grid/GUI.java
@@ -10,11 +10,16 @@ import java.awt.event.*;
 
 public class GUI extends JFrame implements ActionListener{
 
-	int spacing = 5; // Constante qui decide l'ecart entre chaque case
-	GridLayout grid = new GridLayout(20,20,4,3);
-	JButton b2 = new JButton("Menu"); 
+	
+	GridLayout grid = new GridLayout(20,20,3,4);
+	JButton b2 = new JButton("Menu");
 
 	JFrame frame = new JFrame();
+     JPanel panel_menu = new JPanel();
+    
+    //
+
+
 	// La fenetre
 	public GUI(){
 		frame.setTitle("Solveur"); // Titre de la Fenetre
@@ -27,13 +32,20 @@ public class GUI extends JFrame implements ActionListener{
 		frame.setResizable(true); // Permet de resize la fenetre
 		frame.setLayout(new BorderLayout());
 
+         //Panel menu
+        panel_menu.setBackground(Color.blue);
+	    frame.add(panel_menu,BorderLayout.WEST); 
+     	panel_menu.setLayout(new FlowLayout());
+
+     	//
+
 
 		/// Les Boutons
 
 		JButton b1 = new JButton("Solve game");
-        frame.add(b1);
-        b1.setBounds(50,50,200,100); 
-        //b1.setSize(100,100);
+        frame.add(b1,BorderLayout.NORTH);
+        b1.setBounds(500,50,200,100); 
+       // b1.setSize(105,100);
         b1.setVisible(true);
 
 
@@ -41,10 +53,10 @@ public class GUI extends JFrame implements ActionListener{
         /// Bouton avec Action listener
         JButton b2 = new JButton("Menu");
         
-        b2.setBounds(300,50,200,100); 
-        //b2.setSize(100,100);
+        b2.setBounds(1000,50,200,100); 
+       // b2.setSize(100,100);
         b2.addActionListener(this);
-        frame.add(b2);
+        frame.add(b2,BorderLayout.NORTH);
         b2.setVisible(true);
 
 		
@@ -80,7 +92,7 @@ public class GUI extends JFrame implements ActionListener{
 
 		for (int i =0; i<400; i++){ // Boucle permettant de creer les cases
 				JLabel label1 = new JLabel( "mur" );  // Commentaire dans la box
-				label1.setMaximumSize(new Dimension(Integer.MAX_VALUE, label1.getMinimumSize().height)); // 
+				label1.setMaximumSize(new Dimension(50, 50)); // 
 				label1.setOpaque( true ); // Background du label
 				label1.setBackground( Color.GRAY ); // couleur du background du label
 				label1.setForeground( Color.BLACK ); // couleur du texte
@@ -94,12 +106,53 @@ public class GUI extends JFrame implements ActionListener{
 
     /// Methode qui est censé faire "pousser" la grille vers la droite
 	public void actionPerformed(ActionEvent e){
-			if (e.getSource()== b2){
-				Component rigidareaW = Box.createRigidArea(new Dimension(30,30));// "Mur invisible" Situé au OUEST
-				frame.add(rigidareaW, BorderLayout.WEST); // Ajout des "Mur Invisible" OUEST
 
 
-			}
+				Component rigidareaW = Box.createRigidArea(new Dimension(100,100));// "Mur invisible" Situé au OUEST
+				panel_menu.add(rigidareaW, BorderLayout.WEST); // Ajout des "Mur Invisible" OUESt
+
+                
+              
+				JButton bgen = new JButton("Generation");
+        		panel_menu.add(bgen,FlowLayout.LEFT);
+        		bgen.setBounds(8,50,100,100);         		
+        		bgen.setVisible(true);
+
+        		JButton bclean = new JButton("Clean");
+        		panel_menu.add(bclean,FlowLayout.LEFT);
+        		bclean.setBounds(8,50,100,100);        		
+        		bclean.setVisible(true);
+
+                JButton bclose = new JButton("Close");
+        		panel_menu.add(bclose,FlowLayout.LEFT);
+        		bclose.setBounds(8,50,100,100);
+        		bclose.setVisible(true); 
+
+        		panel_menu.setVisible(true);
+
+        		bclose.addActionListener(new ActionListener()
+        		{     		
+        		
+        		
+        		public void actionPerformed(ActionEvent event)
+        		{
+        			panel_menu.setVisible(false);
+        		}
+        	});
+
+        		
+
+
+
+
 
 		}
+
 }
+
+
+// Pour le menu:
+    // pour fermer le menu
+    // Boutton:generation
+    //bouton: clean (mise a zero de la grille)
+    // 
