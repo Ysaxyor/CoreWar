8f6147ca863689d45b219eb430a0651a03dae0c7
Butterpod <59997173+Butterpod@users.noreply.github.com>
2020-03-16 13:56:35 +0100

    Add files via upload

---
+++ solveur/vue/Grid/Main.java
@@ -0,0 +1,14 @@
+package solveur.vue;
+
+import grid.vue.*;
+
+public class Main {
+	public static void main(String[] args) {
+	new Vue();
+	}
+	
+}
+	
+
+
+
---
+++ solveur/vue/Grid/PanelNord.java
@@ -0,0 +1,40 @@
+package solveur.vue;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.Object;
+
+
+public class PanelNord extends JPanel {
+
+	private JButton solve;
+	private JButton menu;
+	private PanelWest pw;
+
+	public PanelNord(PanelWest pw){
+
+		this.pw=pw;
+
+		this.setLayout(new FlowLayout(FlowLayout.CENTER,100,50));
+
+		this.solve=new JButton("Solve Game");
+		this.menu= new JButton("Menu");
+
+		this.add(solve);
+		this.add(menu);
+		this.menu.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				PanelNord.this.pw.setMenu();
+				PanelNord.this.pw.isMenu();
+			}
+
+		});
+
+		this.setVisible(true);
+	}
+
+
+}
