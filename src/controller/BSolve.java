11ecff7b127bfbdc74e6acc1305b5b129c0d6f7d
tolgatr61 <47320521+tolgatr61@users.noreply.github.com>
2020-03-25 01:12:39 +0100

    Fix BSolve + alerte de fin

--- src/controller/BSolve.java
+++ src/controller/BSolve.java
@@ -6,58 +6,56 @@ import src.modele.ia.*;
 
 import javax.swing.*;
 import java.util.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.lang.Object;
+import java.util.concurrent.TimeUnit;
 
 public class BSolve extends JButton implements ActionListener{
 
 
-    private State jeu;
-    private int clc;
-    private ArrayList<Node> listeEtats;
-    public static boolean firstClick=true;
+	private State jeu;
+	private static int clc;
+	private ArrayList<Node> listeEtats;
 
-    public BSolve(State jeu){
+	public BSolve(State jeu){
 
-        super("Solve");
-        this.jeu = jeu;
-        this.clc = 0;
-        this.addActionListener(this);
+		super("Solve");
+		this.jeu = jeu;
+		this.clc = 0;
+		this.addActionListener(this);
 
-    }
-
-
-    public void actionPerformed(ActionEvent e){
+	}
 
 
-        // Premier clique on déclare tout et initialise l'IA du jeu.
-        if (firstClick){
-            Node noeud = new Node(jeu);
-            GraphState g = new GraphState(noeud);
-            IA ia = new IA(jeu);
-            ArrayList<Node> na = ia.aEtoile();
-            this.listeEtats = na;
-            firstClick=false;
-        }
-
-        if (clc < listeEtats.size()){
-
-            // Dans la logique j'aurai fais ca si ca fonctionnait.
-            //this.jeu.getGrille().getEnsemblePiont().clear();
-            //this.jeu.getGrille().setEnsemblePiont(this.listeEtats.get(clc).getValeur().getGrille().getEnsemblePiont());
-
-            Grille new_grille = this.jeu.getGrille();
-            new_grille.setEnsemblePiont(this.listeEtats.get(clc).getValeur().getGrille().getEnsemblePiont());
-            this.jeu.setGrille(new_grille);
-            this.listeEtats.get(clc).getValeur().affiche();
-        }
-
-        else{
-            this.clc=0;
-            System.out.println("Jeu résolu");
-        }
-
-        this.clc += 1;
+	public void actionPerformed(ActionEvent e){
 
 
-    }
+	// Premier clique on déclare tout et initialise l'IA du jeu.
+		if (clc == 0){
+		Node noeud = new Node(jeu);
+		GraphState g = new GraphState(noeud);
+		IA ia = new IA(jeu);
+		ArrayList<Node> na = ia.aEtoile();
+		this.listeEtats = na;
+		this.listeEtats.get(clc).getValeur().affiche();
+	}
+
+		if (clc < listeEtats.size()){
+			
+			Grille new_grille = this.jeu.getGrille();
+			new_grille.setEnsemblePiont(this.listeEtats.get(clc).getValeur().getGrille().getEnsemblePiont());
+			this.jeu.setGrille(new_grille);
+			this.listeEtats.get(clc).getValeur().affiche();
+			
+			}
+			
+		else{
+			this.clc = 0;
+			JOptionPane.showMessageDialog(null, "Jeu resolu !");
+		}
+		
+		this.clc += 1;
+		
+		}
 }
